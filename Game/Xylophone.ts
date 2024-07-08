6c09eed6e4fd8947a73b44c0717e3860fcdbb5a6
Denise <denisealiciafranz@gmail.com>
2024-07-08 13:03:45 +0200

    key creation

--- Game/Xylophone.ts
+++ Game/Xylophone.ts
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 const keyCount = 8;
 let pitches: string[] = ["C","D","E","F", "G", "A", "H", "C'"];
-let colors: string[] = []
+let colors: string[] = ["#ea4029", "#2020b8", "#f3f646", "#42f4e9", "#53ed41", "#b53af3", "#f0af37", "#f360c0"];
 
 canvas.addEventListener("click", handleClick);
 createBoard();
@@ -20,12 +20,16 @@ interface XyloKey {
 
 function createBoard() {
     for (let i: number = 0; i < keyCount; i++) {
+
+        let y: number = 100;
+        let x: number
         
         let newKey: XyloKey = {
-            pitch: pitches[i];
-            width: 100,
-            length: 500,
-            color:
+            pitch: pitches[i],
+            width: 50,
+            length: 400,
+            color: colors[i],
+            posX: 
 
         }
     }
