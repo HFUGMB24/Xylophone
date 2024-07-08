0bc5ca08a6a9a06c101b4f0a99d4372411ea5760
Denise <denisealiciafranz@gmail.com>
2024-07-08 14:18:03 +0200

    xylophone base structure

--- Game/Xylophone.js
+++ Game/Xylophone.js
@@ -2,12 +2,56 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const keyCount = 8;
+let pitches = ["C", "D", "E", "F", "G", "A", "H", "C'"];
+let colors = ["#ea4029", "#2020b8", "#f3f646", "#42f4e9", "#53ed41", "#b53af3", "#f0af37", "#f360c0"];
+let keys = [];
 canvas.addEventListener("click", handleClick);
 createBoard();
 function createBoard() {
+    let rods = new Path2D;
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 3;
+    rods.moveTo(120, 200);
+    rods.lineTo(canvas.width - 180, 200);
+    rods.moveTo(120, canvas.height - 200);
+    rods.lineTo(canvas.width - 180, canvas.height - 200);
+    ctx.stroke(rods);
     for (let i = 0; i < keyCount; i++) {
+        let y = 20 + 20 * i;
+        let x = 150 + 110 * i;
+        let l = 560 - 40 * i;
+        let newKey = {
+            pitch: pitches[i],
+            width: 75,
+            length: l,
+            color: colors[i],
+            posX: x,
+            posY: y,
+            path: new Path2D,
+        };
+        keys.push(newKey);
+        drawKey(newKey);
     }
 }
+function drawKey(_key) {
+    let x = _key.posX;
+    let y = _key.posY;
+    let keyPath = _key.path;
+    ctx.fillStyle = _key.color;
+    ctx.lineWidth = 1;
+    keyPath.moveTo(x, y);
+    keyPath.rect(x, y, _key.width, _key.length);
+    ctx.fill(keyPath);
+    ctx.stroke(keyPath);
+}
 function handleClick(_event) {
+    let x = _event.offsetX;
+    let y = _event.offsetY;
+    for (let i = 0; i < keys.length; i++) {
+        let keyCheck = keys[i];
+        if (ctx.isPointInPath(keyCheck.path, x, y)) {
+            console.log(keyCheck.pitch);
+        }
+    }
 }
 //# sourceMappingURL=Xylophone.js.map
--- Game/Xylophone.ts
+++ Game/Xylophone.ts
@@ -1,8 +1,9 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 const keyCount = 8;
-let pitches: string[] = ["C","D","E","F", "G", "A", "H", "C'"];
+let pitches: string[] = ["C", "D", "E", "F", "G", "A", "H", "C'"];
 let colors: string[] = ["#ea4029", "#2020b8", "#f3f646", "#42f4e9", "#53ed41", "#b53af3", "#f0af37", "#f360c0"];
+let keys: XyloKey[] = [];
 
 canvas.addEventListener("click", handleClick);
 createBoard();
@@ -14,29 +15,72 @@ interface XyloKey {
     color: string,
     posX: number,
     posY: number,
+    path: Path2D,
 }
 
 
 
 function createBoard() {
+
+    let rods: Path2D = new Path2D;
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 3;
+
+    rods.moveTo(120, 200);
+    rods.lineTo(canvas.width - 180, 200);
+    rods.moveTo(120, canvas.height - 200);
+    rods.lineTo(canvas.width - 180, canvas.height - 200);
+
+    ctx.stroke(rods);
+
     for (let i: number = 0; i < keyCount; i++) {
 
-        let y: number = 100;
-        let x: number = 50 + 60*i;
-        
+        let y: number = 20 + 20 * i;
+        let x: number = 150 + 110 * i;
+        let l: number = 560 - 40* i;
+
         let newKey: XyloKey = {
             pitch: pitches[i],
-            width: 50,
-            length: 400,
+            width: 75,
+            length: l,
             color: colors[i],
             posX: x,
             posY: y,
-
+            path: new Path2D,
         }
+
+        keys.push(newKey);
+        drawKey(newKey);
     }
-    
+
+}
+
+function drawKey(_key: XyloKey) {
+    let x: number = _key.posX;
+    let y: number = _key.posY;
+    let keyPath: Path2D = _key.path;
+
+    ctx.fillStyle = _key.color;
+    ctx.lineWidth = 1;
+
+    keyPath.moveTo(x, y);
+    keyPath.rect(x, y, _key.width, _key.length);
+    ctx.fill(keyPath);
+    ctx.stroke(keyPath);
 }
 
 function handleClick(_event: MouseEvent) {
+    let x: number = _event.offsetX;
+    let y: number = _event.offsetY;
+
+    for(let i: number = 0; i < keys.length; i++) {
+
+        let keyCheck: XyloKey = keys[i];
+
+        if (ctx.isPointInPath(keyCheck.path, x, y)) {
+            console.log(keyCheck.pitch);
+        }
+    }
+
 
 }
