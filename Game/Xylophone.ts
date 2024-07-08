a46f370b05f345f5a71040a0abc7711c714770c3
Denise <denisealiciafranz@gmail.com>
2024-07-08 12:17:27 +0200

    hello :)

--- Game/Xylophone.ts
+++ Game/Xylophone.ts
@@ -1,2 +1,20 @@
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
+
+canvas.addEventListener("click", handleClick);
+
+interface XyloKey {
+    pitch: string,
+    size: number,
+    color: string,
+    posX: number,
+    posY: number,
+}
+
+function createBoard() {
+
+}
+
+function handleClick(_event: MouseEvent) {
+
+}
