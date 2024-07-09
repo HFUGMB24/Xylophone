65e0acba15212dc6bed8c2391d50c9190717c5fd
Jussie247 <justus.schultzke@gmail.com>
2024-07-09 09:44:56 +0200

    Deleted startscreen folder

--- Main/Startscreen/startscreen.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-document.addEventListener("DOMContentLoaded", () => {
-    const startButton = document.getElementById("start-game");
-    startButton.addEventListener("click", () => {
-        window.location.href = "Main/Game/Xylophone.html";
-    });
-});
--- Main/Startscreen/startscreen.ts
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const startButton = document.getElementById("start-game") as HTMLButtonElement;
-
-    startButton.addEventListener("click", () => {
-        window.location.href = "Main/Game/Xylophone.html"
-    })
-})
