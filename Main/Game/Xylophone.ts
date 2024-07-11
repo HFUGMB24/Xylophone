c97d5af7d2a02d7679fb6a474d3e4b2ab950bcb7
Denise <denisealiciafranz@gmail.com>
2024-07-11 10:24:37 +0200

    comments

--- Main/Game/Xylophone.ts
+++ Main/Game/Xylophone.ts
@@ -3,17 +3,14 @@ let currentGameMode: string = "Standard";
 document.getElementById("start-game")!.addEventListener("click", startGame);
 const audioCtx = new AudioContext();
 
+//global variables
 let keys: XyloKey[] = [];
 let playerSong: string = "";
 let playerTurn: boolean = false;
 let keysPlayed: number = 0;
 let song: string = "11556654433221";
 let songProgress: number = 0;
-
-declare module "songs.json" {
-    export const name: string;
-    export const notes: string;
-}
+let strikeCount: number = 0;
 
 interface XyloKey {
     sound: string,
@@ -76,7 +73,7 @@ function startGame() {
    initXylophoneGame();
 }
 
-function initXylophoneGame() {
+function initXylophoneGame() { //builds framework for the game
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
@@ -95,6 +92,7 @@ function initXylophoneGame() {
     createBoard();
 
     function createBoard() {
+        //draw the xylophone
         let rods: Path2D = new Path2D;
         ctx.strokeStyle = "black";
         ctx.lineWidth = 3;
@@ -106,6 +104,7 @@ function initXylophoneGame() {
 
         ctx.stroke(rods);
 
+        //generate xylophone keys
         for (let i: number = 0; i < keyCount; i++) {
             let y: number = 20 + 20 * i;
             let x: number = 150 + 110 * i;
@@ -127,11 +126,13 @@ function initXylophoneGame() {
         }
     }
 
-    let tempSong: string = "";
+    
     function handleClick(_event: MouseEvent) {
+        let tempSong: string = "";
         let x: number = _event.offsetX;
         let y: number = _event.offsetY;
 
+        //in standard mode, checks if played note is correct and adds it to the total played notes
         if (currentGameMode === "Standard") {
             if (playerTurn == true) {
                 if (keysPlayed < songProgress) {
@@ -149,13 +150,13 @@ function initXylophoneGame() {
                                 wrongKey();
                                 keysPlayed = keysPlayed - 1;
                             }
-                            else {playerSong = tempSong;}
+                            else {playerSong = tempSong;} //played note is only added if correct
                             
-                            if (keysPlayed >= songProgress) {
+                            if (keysPlayed >= songProgress) { //checks if its time for the computer to play
                                 playerTurn = false;
                             }
 
-                            console.log("Keys played: " + keysPlayed);
+                            //console.log("Keys played: " + keysPlayed);
                             if (playerSong.length == song.length) {
                                 victory();
                             }
@@ -164,7 +165,7 @@ function initXylophoneGame() {
                 }
             }
         }
-        else if (currentGameMode === "Freemode") {
+        else if (currentGameMode === "Freemode") { //in free mode, key is played normally
             for (let i: number = 0; i < keys.length; i++) {
                 let keyCheck: XyloKey = keys[i];
 
@@ -176,7 +177,8 @@ function initXylophoneGame() {
     }
 }
 
-function drawKey(_key: XyloKey, _ctx: CanvasRenderingContext2D) {
+function drawKey(_key: XyloKey, _ctx: CanvasRenderingContext2D) { 
+    //draws the current key
     let x: number = _key.posX;
     let y: number = _key.posY;
     let keyPath: Path2D = _key.path;
@@ -191,12 +193,14 @@ function drawKey(_key: XyloKey, _ctx: CanvasRenderingContext2D) {
 }
 
 function playKey(_key: XyloKey): void {
+    //current key is played
     let sound = new Audio(_key.sound);
     sound.play();
     glowKey(_key);
 }
 
 function glowKey(_key: XyloKey): void {
+    //played key glows
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
     
@@ -214,6 +218,7 @@ function glowKey(_key: XyloKey): void {
 }
 
 function lightenColor(color: string, amount: number): string {
+    //calculates color for glow effect
     const hex = color.replace('#', '');
     const rgb = parseInt(hex, 16);
     const r = Math.min(255, ((rgb >> 16) & 0xff) + amount);
@@ -222,7 +227,7 @@ function lightenColor(color: string, amount: number): string {
     return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
 }
 
-function simonSays(_song: string) {
+function simonSays(_song: string) { //computer plays selected song note for note and waits for the player 
     if (currentGameMode !== "Standard") return;
 
     if (playerTurn == false) {
@@ -235,14 +240,13 @@ function simonSays(_song: string) {
                 playerTurn = true;
                 playerSong = "";
                 keysPlayed = 0;
-
-                console.log(songProgress);
             }
         }
     }
 }
 
 function checkPlayerSong(_song: string, _player: string): boolean {
+    //checks if the player played the correct note
     let playerCorrect: boolean = true;
     let songPart: string = _song.substring(0, keysPlayed);
     if (songPart !== _player) {
@@ -252,8 +256,9 @@ function checkPlayerSong(_song: string, _player: string): boolean {
     return playerCorrect
 }
 
-let strikeCount: number = 0;
+
 function wrongKey(): void {
+    //adds to the strike counter and checks for game over
     strikeCount += 1;
     let strikes: HTMLElement = document.getElementsByTagName("span")[0];
     strikes.textContent = "Strikes: " + String(strikeCount);
@@ -263,6 +268,7 @@ function wrongKey(): void {
 }
 
 function gameOver():void {
+    //game over screen
     document.body.innerHTML = "";
 
     let gameOver: HTMLElement = document.createElement("h1")!;
@@ -280,6 +286,7 @@ function gameOver():void {
 }
 
 function victory(): void {
+    //victory screen
     document.body.innerHTML = "";
 
     let victory: HTMLElement = document.createElement("h1")!;
@@ -296,11 +303,11 @@ function victory(): void {
     document.body.appendChild(restartButton);
 }
 
-function restart(_event: MouseEvent):void {
+function restart(_event: MouseEvent):void { //reloads page
     location.reload();
 }
 
-function songSelector(): void {
+function songSelector(): void { //builds the song select buttons next to xylophone
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     const buttonContainer = document.createElement("div");
     buttonContainer.style.position = "absolute";
@@ -311,6 +318,7 @@ function songSelector(): void {
     buttonContainer.style.gap = "10px";
 
 
+    //generate song select buttons
     let songs: string[] = ["11556654433221", "334554321123322", "321233322233355321233322232", "3213566653213334332321", "5545225875" ]
     let songNames: string[] = ["Twinkle Twinkle Little Star", "Ode to Joy", "Mary had a little Lamb", "Let It Be", "Funky Town (Riff)" ]
     
@@ -328,7 +336,7 @@ function songSelector(): void {
 
 }
 
-function selectSong(_event: MouseEvent) {
+function selectSong(_event: MouseEvent) { //when new song is selected, load it and reset important variables
     song = (_event.target as Element).id;
     playerSong = "";
     keysPlayed = 0;
