e6dfd04a33045c0d20a03de0aa294fe03547c41a
Denise <denisealiciafranz@gmail.com>
2024-07-11 10:05:41 +0200

    button cleanup

--- Main/Game/Xylophone.ts
+++ Main/Game/Xylophone.ts
@@ -309,42 +309,6 @@ function songSelector(): void {
     buttonContainer.style.display = "flex";
     buttonContainer.style.flexDirection = "column";
     buttonContainer.style.gap = "10px";
-    
-
-    let songButton = document.createElement("button");
-    songButton.textContent = "Twinkle Twinkle Little Star";
-    songButton.id = "11556654433221";
-    songButton.className = "songSelect"
-    songButton.addEventListener("click", selectSong);
-    buttonContainer.appendChild(songButton);
-
-    let songButton2 = document.createElement("button");
-    songButton2.textContent = "Ode to Joy";
-    songButton2.id = "334554321123322";
-    songButton2.className = "songSelect"
-    songButton2.addEventListener("click", selectSong);
-    buttonContainer.appendChild(songButton2);
-
-    let songButton3 = document.createElement("button");
-    songButton3.textContent = "Mary had a little Lamp";
-    songButton3.id = "321233322233355321233322232";
-    songButton3.className = "songSelect"
-    songButton3.addEventListener("click", selectSong);
-    buttonContainer.appendChild(songButton3);
-
-    let songButton4 = document.createElement("button");
-    songButton4.textContent = "Let It Be";
-    songButton4.id = "3213566653213334332321";
-    songButton4.className = "songSelect"
-    songButton4.addEventListener("click", selectSong);
-    buttonContainer.appendChild(songButton4);
-
-    let songButton5 = document.createElement("button");
-    songButton5.textContent = "Funky Town (Riff)";
-    songButton5.id = "5545225875";
-    songButton5.className = "songSelect"
-    songButton5.addEventListener("click", selectSong);
-    buttonContainer.appendChild(songButton5);
 
 
     let songs: string[] = ["11556654433221", "334554321123322", "321233322233355321233322232", "3213566653213334332321", "5545225875" ]
@@ -356,7 +320,7 @@ function songSelector(): void {
         songButton.id = songs[i];
         songButton.className = "songSelect";
         songButton.addEventListener("click", selectSong);
-        document.body.appendChild(songButton);
+        buttonContainer.appendChild(songButton);
     }
     
 
